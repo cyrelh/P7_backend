7a92a242dac6097afda2166d7429c630d688a377
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-03-06 19:14:38 +0100

    Envoyée une requte GET individuel pour un unique livre Requete GET images des url bien affichées

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -2,12 +2,42 @@ const { upload } = require('../middleware/multer');
 const { Book } = require('../models/Book');// avec cet User , on va pouvoir utiliser ce modèle de mongo pour pouvoir enregistrer des data en base de données 
 const express = require('express');
 
+const booksRouter = express.Router(); //dans le booksRouter on a seulement 2 requetes à la racine
+booksRouter.get('/:id', BookGETById); //on nomme le params par :id
+booksRouter.get('/', booksGET);
+booksRouter.post('/', upload.single('image'), booksPOST); // quand on fait un post sur le booksRouter, 
+//multer va d'abord etre appele ce middlweare et upload.single('image')
+// puis il va rajouter uen propriété file su rnotre req pour que le booksPOST y est accès
+
+
+async function BookGETById(req, res){
+  const id = req.params.id; // notre requete a une propriété qui s'appelle params
+  // console.log("params:", params); //ça affiche ceci params: { id: '65e8980790e4c4edca39b3a0' }
+  // pas de body dans une req GET mais des params
+  try{
+    const book = await Book.findById(id);
+    console.log("book:", book);
+    if (book == null) {
+      res.status(404).send("Livre non trouvé");// l'id avait une bonne longueur mais pas trouvé dans la base de données
+      return;
+    }
+    book.imageUrl =imagesPath(book.imageUrl) ; // on a besoin de changer l'imageUrl avant de lui envoyer
+    res.send(book);
+  } catch (e){
+    console.error(e);
+    res.status(500).send("Erreur Serveur:" + " " + e.message);
+  }
+  }
+
+
+
+
+
 async function booksGET(req, res){
   const booksDatabase = await Book.find();
   console.log("booksDatabase:", booksDatabase);
 
 // Pour chaque book on va lui rajouter une propriété dessus et on va lui modifier l'imageUrl
-
   booksDatabase.forEach(book => { // pour chacun d'entre eux
     book.imageUrl =imagesPath(book.imageUrl) ; // à la place du book.imageUrl, tu vas mettre le resultat de la fonction imagesPath à partir de l'argument book.imageUrl
   });
@@ -17,6 +47,8 @@ async function booksGET(req, res){
 return process.env.URL + '/' + process.env.IMAGES_PATH + '/'+ nomFichier;
   }
   
+
+
   async function booksPOST(req, res){
     const file = req.file;
     console.log("file:", file);
@@ -38,11 +70,7 @@ return process.env.URL + '/' + process.env.IMAGES_PATH + '/'+ nomFichier;
     }
   }
 
-  const booksRouter = express.Router(); //dans le booksRouter on a seulement 2 requetes à la racine
-  booksRouter.get('/', booksGET);
-  booksRouter.post('/', upload.single('image'), booksPOST); // quand on fait un post sur le booksRouter, 
-  //multer va d'abord etre appele ce middlweare et upload.single('image')
-  // puis il va rajouter uen propriété file su rnotre req pour que le booksPOST y est accès
+
 
       //   Book.deleteMany({}).then(() => {
       //   console.log('Supression de tous les Books dans la Database')
