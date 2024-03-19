ae4c84cba04bb53fb6e567b01e7421f915dbd96d
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-03-20 00:40:43 +0100

    bestrating  GET fonctionnel  mais bookPUT erreur serveur

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -6,11 +6,79 @@ const jwt = require("jsonwebtoken");
 
 
 const booksRouter = express.Router(); //on crée le routeur Express et dans le booksRouter on a seulement 2 requetes à la racine
+
+booksRouter.get("/bestrating", bestRatingGET);
 booksRouter.get('/:id', BookGETById); // Route pour récupérer un livre par son ID  et on nomme le params par :id
 booksRouter.get('/', booksGET); // Route pour récupérer tous les livres
-booksRouter.post('/', auth, upload.single('image'), booksPOST); // Route pour ajouter un livre avec téléchargement d'image
+booksRouter.post('/', auth, upload.single('image'), bookPOST); // Route pour ajouter un livre avec téléchargement d'image
 booksRouter.delete("/:id", auth, booksDELETE);
+booksRouter.put("/:id", auth, upload.single("image"), bookPUT);
 
+async function bestRatingGET(req, res) {
+  try { // au lieu de m'envoyer toute la base de données, on demande à mongoose de m'en envoyer que 3
+    const booksBestRatings = await Book.find().sort({ rating: -1 }).limit(3); // On demande à mongoose de faire le job --> rating: -1  dan sl'ordre décroissant ; Book vient de la fonction Book modele  de mongoose; .sort propriété de mongoose et .limit aussi
+    booksBestRatings.forEach((book) => {
+      book.imageUrl = imagesPath(book.imageUrl); // important sinon on aura pas l'affichage des images
+    });
+    res.send(booksBestRatings);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Erreur Serveur:" + e.message);
+  }
+}
+
+async function bookPUT(req, res) {
+  const id = req.params.id; // on a l'id
+  const book = JSON.parse(req.body.book); // on parse le book en string pour qu'il soit en objet
+  try {
+    const bookDatabase = await Book.findById(id); // on recherche l'id dans la db
+    if (bookDatabase == null) {
+      res.status(404).send("Livre introuvable");
+      return;
+    }
+    const userDatabase = bookDatabase.userId;
+    const userIdToken = req.tokenPayload.userId;
+    if (userDatabase != userIdToken) {
+      res.status(403).send("Vous n'avez pas le droit de modifier le livre de quelqu'un d'autre");
+      return;
+    }
+
+    const newBook = {}; // objet vide, et je veux lui ajouetrle spropriéts que je veux dessus
+    if (book.title) newBook.title = book.title; // On a parsé book avant donc ce n'est plus req.body.book
+    if (book.author) newBook.author = book.author;
+    if (book.year) newBook.year = book.year;
+    if (book.genre) newBook.genre = book.genre;
+    if (req.file != null) newBook.imageUrl = req.file.filename;
+
+    await Book.findByIdAndUpdate(id, newBook); // ça ce sont les nouveaux champs qu'on va mettre dans le nouveau champ qui existe deja
+    res.send("Livre modifié ! ");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Erreur serveur:" + e.message);
+  }
+}
+
+async function booksDELETE(req, res) {
+  const id = req.params.id; // on récupère l'id depuis les req.params
+  try {
+    const bookDatabase = await Book.findById(id);
+    if (bookDatabase == null) { // si l'id du book est inexistant dans la db alor livre n'est pas supprimé
+      res.status(404).send("Livre introuvable");
+      return; // on va pas plus loin
+    }
+    const userDatabase = bookDatabase.userId; // Pour éviter de faire supprimer le livre de quelqu'un d'autre
+    const userIdToken = req.tokenPayload.userId; // ça sert à comparer le userId du token avec l'userId dans la  base de données
+    if (userDatabase != userIdToken) { //si c'est pas le meme token, et bien non on autorise pas la suppression du livre
+      res.status(403).send("Vous n'avez pas le droit de supprimer un livre qui ne vous appartient pas");
+      return;
+    }
+    await Book.findByIdAndDelete(id);
+    res.send("Livre supprimé !");
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Something went wrong:" + e.message);
+  }
+}
 
 function auth(req, res, next) {
   const headers = req.headers; // le auth va regarder dans le headers 
@@ -56,25 +124,7 @@ async function BookGETById(req, res){   // Fonction pour récupérer un livre pa
   }
   }
 
-async function booksGET(req, res){   // Fonction pour récupérer tous les livres
-  try {
-    const booksDatabase = await Book.find();
-    console.log("booksDatabase:", booksDatabase);
-  
-  // Pour chaque book on va lui rajouter une propriété dessus et on va lui modifier l'imageUrl
-    booksDatabase.forEach(book => { // pour chacun d'entre eux
-      book.imageUrl =imagesPath(book.imageUrl) ; // à la place du book.imageUrl, tu vas mettre le resultat de la fonction imagesPath à partir de l'argument book.imageUrl
-    });
-    res.send(booksDatabase); // La réponse de la requete HTTP GET quand on fait appelle à la route api/books
-  } catch (e) {
-    console.error(e);
-    res.status(500).send("Erreur Serveur:" + " " + e.message);
-  } 
-  }
-  function imagesPath (nomFichier){   // Fonction pour générer le chemin complet de l'image
-return process.env.URL + '/' + process.env.IMAGES_PATH + '/'+ nomFichier;
-  }
-  async function booksPOST(req, res){   // Fonction pour ajouter un livre
+async function bookPOST(req, res){   // Fonction pour ajouter un livre
     try{
       if (!req.file) {
         return res.status(400).send('Aucun fichier téléchargé');
@@ -108,27 +158,28 @@ return process.env.URL + '/' + process.env.IMAGES_PATH + '/'+ nomFichier;
     }
   }
 
-  async function booksDELETE(req, res) {
-    const id = req.params.id; // on récupère l'id depuis les req.params
-    try {
-      const bookDatabase = await Book.findById(id);
-      if (bookDatabase == null) { // si l'id du book est inexistant dans la db alor livre n'est pas supprimé
-        res.status(404).send("Livre introuvable");
-        return; // on va pas plus loin
-      }
-      const userDatabase = bookDatabase.userId; // Pour éviter de faire supprimer le livre de quelqu'un d'autre
-      const userIdToken = req.tokenPayload.userId; // ça sert à comparer le userId du token avec l'userId dans la  base de données
-      if (userDatabase != userIdToken) { //si c'est pas le meme token, et bien non on autorise pas la suppression du livre
-        res.status(403).send("Vous n'avez pas le droit de supprimer un livre qui ne vous appartient pas");
-        return;
-      }
-      await Book.findByIdAndDelete(id);
-      res.send("Livre supprimé !");
-    } catch (e) {
-      console.error(e);
-      res.status(500).send("Something went wrong:" + e.message);
-    }
+async function booksGET(req, res){   // Fonction pour récupérer tous les livres
+  try {
+    const booksDatabase = await Book.find();
+    console.log("booksDatabase:", booksDatabase);
+  
+  // Pour chaque book on va lui rajouter une propriété dessus et on va lui modifier l'imageUrl
+    booksDatabase.forEach(book => { // pour chacun d'entre eux
+      book.imageUrl =imagesPath(book.imageUrl) ; // à la place du book.imageUrl, tu vas mettre le resultat de la fonction imagesPath à partir de l'argument book.imageUrl
+    });
+    res.send(booksDatabase); // La réponse de la requete HTTP GET quand on fait appelle à la route api/books
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Erreur Serveur:" + " " + e.message);
+  } 
   }
+  function imagesPath (fileName){   // Fonction pour générer le chemin complet de l'image
+return process.env.URL + '/' + process.env.IMAGES_PATH + '/'+ fileName;
+  }
+
+
+  
+
 
 
       //   Book.deleteMany({}).then(() => { // vider la base de données
