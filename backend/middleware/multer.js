f9ab09fff528e58340a522cf854c32f73af85f32
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-03-20 02:33:57 +0100

    Mise à jour du file name dans multer.js

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,13 +1,20 @@
 // gestion des données formulaires multipart/form-data
 const multer = require('multer');
 
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/webp': 'webp',
+};
+
 const storage = multer.diskStorage({ // appel de diskstorage dans un objet avec ses 2 keys destination et filename
-    destination: function (req, file, cb){
-      cb(null,"uploads");
+    destination: function (req, file, callback){
+      callback(null,"uploads");
     },
-    filename: function (req, file, cb) {
-      const fileName = file.originalname.toLowerCase().split(" ").join("-") + Date.now() + ".webp";
-      cb(null, Date.now() + "-" + fileName);
+    filename: function (req, file, callback) {
+      // const fileName = file.originalname.toLowerCase().split(" ").join("-") + Date.now() + ".webp";
+      callback(null, file.originalname.split(' ').join('_').replace(/\.[^/.]+$/, "") + Date.now() + '.' + MIME_TYPES[file.mimetype]);
     }
   });
   const upload = multer({storage}); // on appelle la fonction multer et on lui passe un obj qui a une key storage et sa valeur est storage: storage
